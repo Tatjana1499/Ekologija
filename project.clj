a39e9f92cee0ec7c29265ec07ceb6e21f5fc96e7
Tatjana1499 <bailovict@gmail.com>
2023-01-03 15:34:04 +0100

    Povezuje sa bazom

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.2.27.jre7"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler ekologija.handler/app}
   :profiles
